db8006085533628689f136566e80f2faf9bf5347
Igor Kartel <igor.v.kartel@gmail.com>
2024-10-22 12:17:51 +0300

    feat: error handling and readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# UpTraderTask
+# UpTrader Task
+
+## Quickstart
+
+1. To set up the project please create new directory **UpTraderTask** on your computer and clone the git repository using the command:
+
+```console
+$ git clone https://github.com/igorkartel/UpTraderTask.git .
+```
+
+2. Then open the project in your IDE (PyCharm or VSCode). A virtual environment for this project must be built with **poetry**.
+Find the file **pyproject.toml** with all dependencies and change the python version you are using in this line:
+**python = "^3.11"** (you can choose "3.10", "3.11" or "3.12" only; I used "3.12", so it is configured as default).
+
+3. Create in the project root your **.env** file. You can see the parameters to define in the file **env_config.py**.
+
+4. Create poetry virtual environment and install the dependencies.
+
+5. Then run in your IDE console the following command to initialize a container with all the services:
+
+```console
+$ docker compose up -d --build
+```
+
+6. Now the project is ready for use.
+You can see generated docs by following links: http://localhost:8003
--- tree_coded_menu/templatetags/menu_tags.py
+++ tree_coded_menu/templatetags/menu_tags.py
@@ -1,6 +1,8 @@
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Prefetch
 
+from config.logger import logger
 from tree_coded_menu.models import MenuItem
 
 register = template.Library()
@@ -11,27 +13,35 @@ def draw_menu(context, menu_name):
     """Рисует древовидное меню с раскрытием активных элементов."""
     current_url = context["request"].path
 
-    # Загружаем корневые пункты меню с prefetch для подменю
-    menu_items = MenuItem.objects.filter(menu_name=menu_name, parent__isnull=True).prefetch_related(
-        Prefetch("children", queryset=MenuItem.objects.all())
-    )
+    try:
+        # Загружаем корневые пункты меню с prefetch для подменю
+        menu_items = MenuItem.objects.filter(menu_name=menu_name, parent__isnull=True).prefetch_related(
+            Prefetch("children", queryset=MenuItem.objects.all())
+        )
 
-    def mark_active_items(items):
-        """Рекурсивно помечает активные элементы меню и раскрывает вложенные уровни."""
-        found_active = False  # Флаг для отслеживания, найден ли активный элемент
-        for item in items:
-            item.active = current_url.startswith(item.get_url())
-            if item.active:
-                found_active = True
-                item.expand = True  # Раскрываем этот элемент
-            if item.children.exists():
-                # Проверяем детей на активность
-                child_active = mark_active_items(item.children.all())
-                if child_active:
-                    item.expand = True  # Если активен любой потомок, раскрываем родителя
+        def mark_active_items(items):
+            """Рекурсивно помечает активные элементы меню и раскрывает вложенные уровни."""
+            found_active = False  # Флаг для отслеживания, найден ли активный элемент
+            for item in items:
+                item.active = current_url.startswith(item.get_url())
+                if item.active:
                     found_active = True
-        return found_active
+                    item.expand = True  # Раскрываем этот элемент
+                if item.children.exists():
+                    # Проверяем детей на активность
+                    child_active = mark_active_items(item.children.all())
+                    if child_active:
+                        item.expand = True  # Если активен любой потомок, раскрываем родителя
+                        found_active = True
+            return found_active
 
-    mark_active_items(menu_items)
+        mark_active_items(menu_items)
 
-    return {"menu_items": menu_items}
+        return {"menu_items": menu_items}
+
+    except ObjectDoesNotExist:
+        logger.error(f"Menu with name '{menu_name}' not found")
+        return {"menu_items": []}
+    except Exception as e:
+        logger.error(f"Error while loading menu '{menu_name}': {e}")
+        return {"menu_items": []}
